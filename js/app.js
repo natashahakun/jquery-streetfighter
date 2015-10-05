739b5f4512cd79343ae4b03433ff3ff993808bd2
natashalynne <hakun.natasha@gmail.com>
2015-10-05 16:40:40 -0400

    Add hadouken sound.

--- js/app.js
+++ js/app.js
@@ -1,28 +1,34 @@
-$(document).ready(function() {
-  $(".ryu").mouseenter(function() {
+$(document).ready(function(){
+  $(".ryu").mouseenter(function(){
     $(".ryu-still").hide();
     $(".ryu-ready").show();
   })
-  .mouseleave(function() {
+  .mouseleave(function(){
     $(".ryu-ready").hide();
     $(".ryu-still").show();
   })
-  .mousedown(function() {
-    // play hadouken sound
+  .mousedown(function(){
+    playHadouken();
     $(".ryu-ready").hide();
     $(".ryu-throwing").show();
     $(".hadouken").finish().show().animate(
       {"left": "1020px"},
       500,
-      function() {
+      function(){
         $(this).hide();
         $(this).css("left", "520px");
-      }
-    );
+      });
   })
-  .mouseup(function() {
-    // ryu goes back to ready position
+  .mouseup(function(){
     $(".ryu-throwing").hide();
     $(".ryu-ready").show();
   });
+
 });
+
+function playHadouken(){
+  $("#hadouken-sound")[0].volume = 0.5;
+  $("#hadouken-sound")[0].load();
+  $("#hadouken-sound")[0].play();
+}
+
